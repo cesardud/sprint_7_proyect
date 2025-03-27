e3dc0dea0a2a5d6c42b7a6f067e66da207956d41
cesardud <cesardudleynieto@gmail.com>
2025-03-27 14:03:02 -0600

    Agregar visualizaciones y archivo de configuración para Render

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # sprint_7_proyect
+## Análisis de venta de vehiculos en Estados Unidos
+Esta es una aplicación web interactiva programada en Python que explorar visualmente un conjunto de datos de anuncios de coches en venta.
+
+#Funciones
+- Muestra Muestra un histográma del odómetro (kilometraje) de los vehiculos.
+- Muestra un gráfico de disersión entre odómetro y precio.
+ 
+La app utiliza las librerías: pandas, plotly-express y streamlit.
---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+car_data = pd.read_csv("/Users/cesardudleycastellanosnieto/Desktop/TT/sprint_7/sprint_7_proyect/vehicles_us.csv")  # subir una carpeta porque el archivo está en la raíz del proyecto
+st.header("Análisis de vehículos en venta en E.U.A")
+
+hist_button = st.button('Construir histograma') # crear un botón
+if hist_button:
+    st.write("Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
+    fig_hist = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+disp_button = st.button('Construir gráfico de dispersión')
+if disp_button:
+    st.write("Creación de una gráfica de dispersión para el conjunto de datos de anuncios de venta de coches")
+    fig_disp =  px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig_disp, use_container_width=True)
+
+build_hist = st.checkbox('Mostrar histograma')
+build_disp = st.checkbox('Mostrar gráfico de dispersión')
+
+if build_hist:
+    st.write("Histograma de odómetro")
+    fig_hist = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+if build_disp:
+    st.write("Dispersión entre odómetro y precio")
+    fig_disp = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig2, use_container_width=True)
